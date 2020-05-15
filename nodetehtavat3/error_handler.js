7e6a47ddc53b19aea64ebda8714beefc50ab49b0
Ooke Nivalainen <ooke.nivalainen@gmail.com>
2020-05-15 19:22:49 +0300

    lisätty nodetehtävät4

--- nodetehtavat3/error_handler.js
+++ nodetehtavat3/error_handler.js
@@ -1,4 +1,4 @@
-const errorHandler = (error, req, res, next) => {
+const errorHandler = (err, req, res, next) => {
     if (!err.statusCode|| err.statusCode=== 500) {
         res.status(500).send({
              error: 'Somethingfailed!' 
